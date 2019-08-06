3e503ae076d2ae0a5528f13c9835772a1586c4ba
Tolmain <adam.stogsdill@gmail.com>
2019-08-06 13:05:16 -0500

    Testing somoe stuff

--- bin/main/project/StatisticsBackend.py
+++ bin/main/project/StatisticsBackend.py
@@ -4,7 +4,8 @@ from numpy import asarray, dot, sort, floor
 class LocalDataset:
 
     def __init__(self, **dataset):
-        for key, value in dataset:
+        """ Creates a local dataset based off of the keys and values of those keys given by the user"""
+        for key, value in dataset.items():
             insertion_information = asarray(value)
             setattr(self, key, insertion_information)
 
@@ -15,10 +16,23 @@ class LocalDataset:
     def q_information(self, arg1):
         """ Returns quartile information about the dataset """
         operating_dataset = getattr(self, arg1)
-        if operating_dataset.dtype == 'int32':
+        if operating_dataset.dtype == 'int32' and operating_dataset.size > 3:
             sorted_set = sort(operating_dataset)
-            median_index = floor(sorted_set.size / 2)
-            q1 = floor(median_index / 2)
-            q3 = floor(sorted_set.size)
+            if type(sorted_set.size / 2) == 'int':
+                median_value = ((sorted_set.tolist()[floor(sorted_set.size / 2)] + sorted_set.tolist()[
+                    floor(sorted_set.size) / 2 + 1]) / 2)
+            else:
+                median_value = sorted_set.tolist()[sorted_set.size / 2]
+            if type(sorted_set.size / 4) == 'int':
+                q1_value = (sorted_set.tolist()[floor(sorted_set.size / 4)] + sorted_set.tolist()[
+                    floor(sorted_set.size) / 4 + 1]) / 2
+            else:
+                median_value = sorted_set.tolist()[sorted_set.size / 4]
+            return median_value, q1_value
+        return False
+
+    #def mean_squared_error(self, arg1, arg2):
+
+
 
 
